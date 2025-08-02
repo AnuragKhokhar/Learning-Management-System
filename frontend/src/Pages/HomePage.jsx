409a8c33cd198e646c77c5b679a2547910a33147
AnuragKhokhar <anuragkhokhar635@gmail.com>
2025-08-02 19:41:52 +0530

    fixed

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,9 +1,20 @@
-import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
 import HomePageImage from "../Assets/Images/homePageMainImage.png";
 import HomeLayout from "../Layouts/HomeLayout";
 
 function HomePage() {
+     const navigate = useNavigate();
+    const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+
+    const handleExploreClick = () => {
+        if (isLoggedIn) {
+            navigate("/courses");
+        } else {
+            alert("Please login to explore courses.");
+        }
+    };
     return (
         <HomeLayout>
             <div className="pt-10 text-white flex items-center justify-center gap-10 mx-16 h-[90vh]">
@@ -19,11 +30,12 @@ function HomePage() {
                     </p>
 
                     <div className="space-x-6">
-                        <Link to="/courses">
-                            <button className="bg-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300">
-                                Explore courses
-                            </button>
-                        </Link>
+                        <button
+                            onClick={handleExploreClick}
+                            className="bg-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300"
+                        >
+                            Explore courses
+                        </button>
 
                         <Link to="/contact">
                             <button className="border border-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300">
